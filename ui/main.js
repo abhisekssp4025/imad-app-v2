553ae423629dd0bc26cf406acafb36de31076062
abhisekssp4025 <abhisekssp4025@gmail.com>
2017-02-16 13:42:32 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,3 +2,8 @@ console.log('Loaded!');
 // change the text of the main-text div
 var element = document.getElementById('main-text');
 element.innerHTML = 'New value';
+//move image
+var img = document.getElementById('madi');
+madi.onclick = function() {
+    madi.style.marginleft = '100px';
+}
